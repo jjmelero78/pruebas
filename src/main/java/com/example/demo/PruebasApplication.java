ba69d4c819343bf02771941fc547a68811662b4d
jjmelero <xmelero78@hotmail.com>
2017-10-01 01:50:49 +0200

    cambio

--- src/main/java/com/example/demo/PruebasApplication.java
+++ src/main/java/com/example/demo/PruebasApplication.java
@@ -2,11 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages={"com.example.demo"})
-public class PruebasApplication {
+public class PruebasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebasApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<PruebasApplication> applicationClass = PruebasApplication.class;
 }
